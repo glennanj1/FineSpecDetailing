c1c7dc9c7f7957e2f276f2ddaf4975b5dd8c91ec
glennanj1 <glennan20@gmail.com>
2022-05-06 14:46:47 -0400

    styling changes

--- finespecdetailing/pages/index.js
+++ finespecdetailing/pages/index.js
@@ -57,11 +57,10 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Fine Spec Detailing{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
