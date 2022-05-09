3f6636433761c1fb959ff17ce8fe092e67b46fec
glennanj1 <glennan20@gmail.com>
2022-05-09 10:20:24 -0400

    Feature Page

---
+++ finespecdetailing/pages/Features.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Features() {
+  return (
+    <div>
+        <h1>
+            Features
+        </h1>
+        <h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </h2>
+        <h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </h2>
+        <h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </h2>
+    </div>
+  )
+}
