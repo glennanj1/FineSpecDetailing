46cbac3f9d54141a254f1fa80b19e9778870c6f4
glennanj1 <glennan20@gmail.com>
2022-05-09 10:20:37 -0400

    Legal Page

---
+++ finespecdetailing/pages/Legal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Legal() {
+  return (
+    <div>
+        <h1>Legal</h1>
+        <h2>Privacy policy</h2>
+        <h2>Terms of use</h2>
+    </div>
+  )
+}
