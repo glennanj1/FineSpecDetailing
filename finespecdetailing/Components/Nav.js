953fe1c9d813733b78dcb0e33868d71c28206326
glennanj1 <glennan20@gmail.com>
2022-05-10 12:39:23 -0400

    nav changes

---
+++ finespecdetailing/Components/Nav.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
+
+export default function Nav() {
+  return (
+    <>
+    <Container maxWidth="md">
+         <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+            Fine Spec Detailing
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="/Features"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Features
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="/Reviews"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              Reviews
+            </Link>
+            <Link
+              variant="button"
+              color="text.primary"
+              href="/FAQ"
+              sx={{ my: 1, mx: 1.5 }}
+            >
+              FAQ
+            </Link>
+          </nav>
+          <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Container>
+    </>
+  )
+}
