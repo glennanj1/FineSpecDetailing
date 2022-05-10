e070bdaa74de351b96c9293caf22c2e3bc6f9b1c
glennanj1 <glennan20@gmail.com>
2022-05-10 12:38:14 -0400

    index cleanup

--- finespecdetailing/pages/index.js
+++ finespecdetailing/pages/index.js
@@ -1,17 +1,10 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
 import Pricing from '../Components/Pricing';
 
 export default function Index() {
   return (
-    <Container maxWidth="md">
-      <Link href="/">
-      </Link>
+    <Container maxWidth="lg">
       <Pricing />
     </Container>
   );
