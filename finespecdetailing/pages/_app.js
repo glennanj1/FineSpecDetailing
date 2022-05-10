0658ef2e8fc84c9cc21281988a5bf1b6eca55cfc
glennanj1 <glennan20@gmail.com>
2022-05-10 12:37:14 -0400

    added nav and footer to app js

--- finespecdetailing/pages/_app.js
+++ finespecdetailing/pages/_app.js
@@ -6,7 +6,9 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
-
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import Nav from '../Components/Nav';
+import Footer from '../components/Footer'
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
@@ -21,7 +23,9 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+          <Nav />
+            <Component {...pageProps} /> 
+          <Footer />
       </ThemeProvider>
     </CacheProvider>
   );
