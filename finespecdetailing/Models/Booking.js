16983a7eb2e37bc6be1f45342cb328dd0567ac0e
glennanj1 <glennan20@gmail.com>
2022-05-14 11:51:30 -0400

    fixed schema

--- finespecdetailing/Models/Booking.js
+++ finespecdetailing/Models/Booking.js
@@ -4,17 +4,14 @@ const BookingSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide a name for this pet.'],
-    maxlength: [60, 'Name cannot be more than 60 characters'],
   },
   email: {
     type: String,
     required: [true, "Please provide the pet owner's name"],
-    maxlength: [60, "Owner's Name cannot be more than 60 characters"],
   },
   phone: {
     type: String,
     required: [true, 'Please specify the species of your pet.'],
-    maxlength: [40, 'Species specified cannot be more than 40 characters'],
   },
   service: {
     type: String,
@@ -26,7 +23,6 @@ const BookingSchema = new mongoose.Schema({
     type: String,
   },
   make: {
-    required: [true, 'Please provide an image url for this pet.'],
     type: String,
   },
   model: {
