895a50ad44923eab1a6a7c65cd967dcf4e93e9f5
glennanj1 <glennan20@gmail.com>
2022-05-16 11:47:27 -0400

    next auth setup from docs

---
+++ finespecdetailing/pages/api/auth/[...nextauth].js
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
+
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+
+  ],
+  theme: {
+    colorScheme: "light",
+  },
+  callbacks: {
+
+    async session({ session, user, token }) {
+      return session
+    },
+  },
+})
