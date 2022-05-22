7d23a05713d77ccf702dd73c6ff0ababbb6116bc
glennanj1 <glennan20@gmail.com>
2022-05-22 15:06:15 -0400

    camera zoom + style changes

--- finespecdetailing/pages/index.js
+++ finespecdetailing/pages/index.js
@@ -6,14 +6,14 @@ import Porche from '../Components/Car.js';
 export default function Index() {
   return (
     <Container maxWidth="lg">
-      <main>
-      <Canvas camera={{zoom: 100, position: [0, 0, 100]}}>
-      
-            <ambientLight intensity={1} />
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <main style={{top: '100px', position: 'fixed', zIndex: '-1000', display: 'flex', justifyContent: 'center', alignContent: 'center'}}>
+      <Canvas 
+      style={{background: 'black', width: '40vw', height: '100vh', borderRadius: '100px', border: 'solid 10px silver', paddingTop: '10px'}} 
+      camera={{zoom: 20, position: [0, 0, 50]}}>
+            <ambientLight intensity={.5} />
+            <spotLight position={[10, 10, 10]} angle={0.25} penumbra={1} />
             <pointLight position={[-10, -10, -10]} />
             <Porche />
-        
         </Canvas> 
       </main>
       <Pricing />
