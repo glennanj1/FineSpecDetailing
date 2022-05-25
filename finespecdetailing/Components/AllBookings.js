07b3f84232ee7319ff9f077083fbcad602b171a5
glennanj1 <glennan20@gmail.com>
2022-05-25 16:13:39 -0400

    added admin logic plus future setup for bookings

--- finespecdetailing/Components/AllBookings.js
+++ finespecdetailing/Components/AllBookings.js
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import { useSession, getSession } from "next-auth/react"
-import { useRouter } from 'next/router'
-
+import DataGrid from './BookingTable'
+import Container from '@mui/material/Container';
 export default function AllBookings() {
     const { data: session, status } = useSession()
     const [bookings, setBookings] = useState([])
     const [loading, setLoading] = useState(true)
     useEffect(() => {
+     
         fetch('/api/bookings/')
         .then(res => {
             setLoading(true);
@@ -27,66 +28,73 @@ export default function AllBookings() {
     }, [setBookings, setLoading])
     
     if (status === "loading") {
-        return <p>Loading...</p>
+        return( <Container component="main" maxWidth="md"> 
+                    <h1>Loading ...</h1>
+                </Container>)
     }
     if (status === "unauthenticated") {
-        return <p>Please Sign in before booking.</p>
+        return( <Container component="main" maxWidth="md"> 
+                    <h1>Please Login to Continue</h1>
+                </Container>)
     }
     if (status === "authenticated") {
-    if (session.status === 'admin') {
-        return (
-        <h1>hey admin</h1>) 
-    } else {
         return (
             <>
-                {!loading || bookings.length >= 1 ? bookings.map(b => (
+                {session.status === 'admin' ?   <DataGrid /> : 
+                    
+                    (<>
+                        <h1>Hey {session.user.email}</h1>
+                        {!loading && bookings.length >= 1 ? bookings.map(b => (
                     <ul>
-                    <li>
-                    <div key={b.id}>
-                        <h5>
-                        {b.id}
-                        </h5>
-                        <h5>
-                        {b.name}
-                        </h5>
-                        <h5>
-                        {b.email}
-                        </h5>
-                        <h5>
-                        {b.phone}
-                        </h5>
-                        <h5>
-                        {b.service}
-                        </h5>
-                        <h5>
-                        {b.accepted}
-                        </h5>
-                        <h5>
-                        {b.year}
-                        </h5>
-                        <h5>
-                        {b.make}
-                        </h5>
-                        <h5>
-                        {b.model}
-                        </h5>
-                        <h5>
-                        {b.appointment}
-                        </h5>
-                        <h5>
-                        {b.paid}
-                        </h5>
-                    </div>
-                    </li>
-                    </ul>   
-                ))
-                : (null)
-                }
+                        <li>
+                        <div key={b.id}>
+                            <h5>
+                            {b.id}
+                            </h5>
+                            <h5>
+                            {b.name}
+                            </h5>
+                            <h5>
+                            {b.email}
+                            </h5>
+                            <h5>
+                            {b.phone}
+                            </h5>
+                            <h5>
+                            {b.service}
+                            </h5>
+                            <h5>
+                            {b.accepted}
+                            </h5>
+                            <h5>
+                            {b.year}
+                            </h5>
+                            <h5>
+                            {b.make}
+                            </h5>
+                            <h5>
+                            {b.model}
+                            </h5>
+                            <h5>
+                            {b.appointment}
+                            </h5>
+                            <h5>
+                            {b.paid}
+                            </h5>
+                        </div>
+                        </li>
+                        </ul>   
+                    ))
+                    : (<><h2>Currently No Bookings At this Time!</h2> <button>Book Now?</button></>)
+                    }
+                    </>)}
+              
+                
             </>
             
         )
     }
         
-    }
+    
         
 }
