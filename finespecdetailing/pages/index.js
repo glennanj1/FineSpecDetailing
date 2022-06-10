bcadf8a041f2ab99c714ef32ad0aa46b3ce53570
glennanj1 <glennan20@gmail.com>
2022-06-10 16:47:25 -0600

    added styling and video

--- finespecdetailing/pages/index.js
+++ finespecdetailing/pages/index.js
@@ -1,11 +1,37 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Pricing from '../Components/Pricing';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Pricing from "../Components/Pricing";
+import styles from "../Styles/index.module.css";
 
 export default function Index() {
+
   return (
-    <Container maxWidth="lg" style={{backgroundImage: 'url(/porche.png)', backgroundPosition: 'bottom', backgroundAttachment: 'fixed', backgroundSize: 'contain'}}>
-      <Pricing />
-    </Container>
+    <>
+      <div className={styles.video}>
+            <video
+              autoPlay
+              playsInline
+              defaultmuted="true"
+              muted
+              loop
+              style={{
+                width: "100vw",
+                height: "100vh",
+                top: "0",
+                position: "absolute",
+                zIndex: "-1",
+                borderRadius: '50%'
+              }}
+            >
+              <source
+                src="https://finespecdetailing.b-cdn.net/drippy.mp4"
+                type="video/mp4"
+              />
+            </video>
+      </div>
+      <Container maxWidth="lg">
+        <Pricing />
+      </Container>
+    </>
   );
 }
