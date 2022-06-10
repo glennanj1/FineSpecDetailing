22b98a35461b8a8805c50876dbb0cc189fb45d22
glennanj1 <glennan20@gmail.com>
2022-06-10 16:49:27 -0600

    formatting

--- finespecdetailing/pages/index.js
+++ finespecdetailing/pages/index.js
@@ -4,30 +4,29 @@ import Pricing from "../Components/Pricing";
 import styles from "../Styles/index.module.css";
 
 export default function Index() {
-
   return (
     <>
       <div className={styles.video}>
-            <video
-              autoPlay
-              playsInline
-              defaultmuted="true"
-              muted
-              loop
-              style={{
-                width: "100vw",
-                height: "100vh",
-                top: "0",
-                position: "absolute",
-                zIndex: "-1",
-                borderRadius: '50%'
-              }}
-            >
-              <source
-                src="https://finespecdetailing.b-cdn.net/drippy.mp4"
-                type="video/mp4"
-              />
-            </video>
+        <video
+          autoPlay
+          playsInline
+          defaultmuted="true"
+          muted
+          loop
+          style={{
+            width: "100vw",
+            height: "100vh",
+            top: "0",
+            position: "absolute",
+            zIndex: "-1",
+            borderRadius: "50%",
+          }}
+        >
+          <source
+            src="https://finespecdetailing.b-cdn.net/drippy.mp4"
+            type="video/mp4"
+          />
+        </video>
       </div>
       <Container maxWidth="lg">
         <Pricing />
