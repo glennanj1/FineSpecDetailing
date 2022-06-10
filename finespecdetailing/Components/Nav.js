3539d6070a989e12c55c772bb2a70a36e752cb1e
glennanj1 <glennan20@gmail.com>
2022-06-10 16:47:12 -0600

    nav update

--- finespecdetailing/Components/Nav.js
+++ finespecdetailing/Components/Nav.js
@@ -1,65 +1,41 @@
-import React from 'react'
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Link from '@mui/material/Link';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+// import AdbIcon from '@mui/icons-material/Adb';
+// import Link from '@mui/material/Link';
 import { useSession, signIn, signOut } from "next-auth/react"
-import fineLogo from '../public/fineLogo.png'
-import Image from 'next/image'
+import { useRouter } from 'next/router';
 
-export default function Nav() {
+// import fineLogo from '../public/fineLogo.png'
+
+// import Image from 'next/image'
+// import { Router } from 'next/router';
+
+
+
+const ResponsiveAppBar = () => {
   const { data: session } = useSession()
-  return (
-    <>
-    <Container maxWidth="md">
-         <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-      >
-        <Toolbar sx={{ flexWrap: 'wrap' }}>
-          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-            <Link href="/">
-              <Image src={fineLogo} width={150} height={30} layout="fixed" />
-              {/* Fine Spec Detailing */}
-            </Link>
-          </Typography>
-          <nav>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="/Features"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Features
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="/Reviews"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Reviews
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="/FAQ"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              FAQ
-            </Link>
-          </nav>
-          {session ? (
+  const router = useRouter()
+
+
+  React.useEffect(() => {
+    console.log(session)
+  }) 
+  const pages = ['About', 'Services', 'Reviews'];
+  const settings = ['Profile', 'Bookings', session ? (
             <>
             <Button onClick={() => signOut()} variant="outlined" sx={{ my: 1, mx: 1.5 }}>
               Logout
-            </Button>
-            <Button href="/bookings" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
-             Bookings
             </Button>
             </>
           ) : 
@@ -67,10 +43,141 @@ export default function Nav() {
             <Button onClick={() => signIn()} variant="outlined" sx={{ my: 1, mx: 1.5 }}>
               Login
             </Button>
-          )}
+          )];
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handlePicClick = () => {
+    router.push('/')
+  }
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Container component="div" sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} >
+            {/* <Image src={fineLogo} width={150} height={30} layout="fixed" onClick={handlePicClick} /> */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex', paddingLeft: '30px'},
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Detailing Done Right 🔥
+          </Typography>
+          </Container>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Container component="div" sx={{ display: { xs: 'flex', md: 'none' }, mr: 1  }} >
+            {/* <Image src={fineLogo} width={150} height={30} layout="fixed" onClick={handlePicClick} /> */}
+          </Container>
+      
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt={session ? session.user.email : null} src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          
         </Toolbar>
-      </AppBar>
-    </Container>
-    </>
-  )
-}
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;
