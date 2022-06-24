405118647352fc08692c9bcd7267e2191080aefc
glennanj1 <glennan20@gmail.com>
2022-06-23 22:27:12 -0400

    Profile Page

---
+++ finespecdetailing/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from "@mui/material/Container";
+import styles from "../Styles/Profile.module.css"
+
+export default function Profile() {
+  return (
+    <>
+      <Container component="main" maxWidth="md" className={styles.Container}>
+        <h1>Welcome to your profile page ... coming soon</h1>
+      </Container>
+    </>
+  )
+}
