c31ebce4a2a400aa7eaaee13e268d202f1af22e4
glennanj1 <glennan20@gmail.com>
2022-06-26 12:01:47 -0400

    facebook auth ✅

--- finespecdetailing/pages/api/auth/[...nextauth].js
+++ finespecdetailing/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import EmailProvider from "next-auth/providers/email"
+import FacebookProvider from "next-auth/providers/facebook";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "../../../lib/mongodb"
 
@@ -15,6 +16,10 @@ export default NextAuth({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
       // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
     }),
 
   ],
