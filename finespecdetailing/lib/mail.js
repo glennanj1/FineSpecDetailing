e73e7fef698a58bb2e275f7dc9687f50a94a621f
glennanj1 <glennan20@gmail.com>
2023-03-07 12:03:06 -0500

    mail module

---
+++ finespecdetailing/lib/mail.js
@@ -0,0 +1,63 @@
+let nodemailer = require('nodemailer')
+
+async function mail (email, body, subject, appointment) {
+console.log('mailing');
+const transporter = nodemailer.createTransport({
+    host: process.env.host,
+    port: 587,
+    auth: {
+    user: process.env.user, // generated ethereal user
+    pass: process.env.pass, // generated ethereal password
+    },
+});
+const mailData = {
+    from: process.env.user,
+    to: email,
+    subject: subject,
+    text: body,
+    attachments: [
+        {   // file on disk as an attachment
+            filename: 'event',
+            path: `/tmp/${appointment._id}.ics` // stream this file
+        },
+        {   // filename and content type is derived from path
+            path: `/tmp/${appointment._id}.ics`
+        },
+    ],
+}
+await transporter.sendMail(mailData).catch(e => console.log(e));
+console.log('success')
+}
+async function mailAdmin (body, subject, appointment) {
+console.log('mailing');
+const transporter = nodemailer.createTransport({
+    host: process.env.host,
+    port: 587,
+    auth: {
+    user: process.env.user, // generated ethereal user
+    pass: process.env.pass, // generated ethereal password
+    },
+});
+const mailData = {
+    from: process.env.user,
+    to: process.env.user,
+    subject: subject,
+    text: body,
+    attachments: [
+        {   // file on disk as an attachment
+            filename: 'event',
+            path: `/tmp/${appointment._id}.ics` // stream this file
+        },
+        {   // filename and content type is derived from path
+            path: `/tmp/${appointment._id}.ics`
+        },
+    ],
+}
+await transporter.sendMail(mailData).catch(e => console.log(e));
+console.log('success');
+}
+
+module.exports = {
+    mail,
+    mailAdmin
+}
