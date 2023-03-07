40537eddb7b3c5839eec97ee8d75a0128f7f7211
glennanj1 <glennan20@gmail.com>
2023-03-07 12:03:25 -0500

    changes

--- finespecdetailing/pages/bookings.js
+++ finespecdetailing/pages/bookings.js
@@ -9,7 +9,7 @@ export default function Bookings() {
     <>
     <Container maxWidth="md">
       <main className={styles.main}>        
-              <AllBookings />
+        <AllBookings />
       </main>
     </Container>
   </>
