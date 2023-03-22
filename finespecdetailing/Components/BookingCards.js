288662436fa4bc73b62cb84c18b8cda309294963
glennanj1 <glennan20@gmail.com>
2023-03-22 00:25:13 -0400

    made booking cards

---
+++ finespecdetailing/Components/BookingCards.js
@@ -0,0 +1,64 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Box } from "@mui/system";
+
+export default function BookingCards({booking}) {
+    debugger;
+  const date = new Date(booking?.appointment);
+  const month = date.toLocaleString("default", { month: "long" });
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const time = date.toLocaleString("en-US", "HH:MM", { hour12: true });
+
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+    >
+      •
+    </Box>
+  );
+
+  return (
+    <>
+      <Card variant="outlined">
+        <>
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 32 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              {booking?.service}
+            </Typography>
+            <Typography sx={{mb: 1.5}} variant="h5" component="div">
+              Status {bull} {booking?.accepted ? "Accepted" : "Pending"}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              Make {booking?.make}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              Model {booking?.model}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              Year {booking?.year}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.primary">
+              {month}-{day}-{year} {bull} {time}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary"></Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Calendar Invite Download</Button>
+          </CardActions>
+        </>
+      </Card>
+    </>
+  );
+}
