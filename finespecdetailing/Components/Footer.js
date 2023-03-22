a2265f1c19f41f3628c428b40515beab3071bfa2
glennanj1 <glennan20@gmail.com>
2023-03-22 00:25:29 -0400

    fixed footer + formatted

--- finespecdetailing/Components/Footer.js
+++ finespecdetailing/Components/Footer.js
@@ -1,61 +1,70 @@
-import React from 'react'
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import EmailIcon from '@mui/icons-material/Email';
-import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
-import IconButton from '@mui/material/IconButton';
-import styles from '../Styles/Footer.module.css'
-import TikTok from './TikTok'
+import React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import IconButton from "@mui/material/IconButton";
+import styles from "../Styles/Footer.module.css";
+import TikTok from "./TikTok";
 function Copyright(props) {
-    return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
-        {'Copyright © '}
-        <a href="https://glennan.netlify.app" color="inherit" style={{textDecoration: 'none', color: 'silver'}}>
-          John Glennan
-        </a>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <a
+        href="https://glennan.netlify.app"
+        color="inherit"
+        style={{ textDecoration: "none", color: "silver" }}
+      >
+        John Glennan
+      </a>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 const footers = [
-    {
-      title: 'About',
-      description: ['Team', 'History', 'Contact us', 'Locations'],
-    },
-    {
-      title: 'Features',
-      description: [
-        'Cleaning',
-      ],
-    },
-    {
-      title: 'Resources',
-      description: ['FAQ'],
-    },
-    {
-      title: 'Legal',
-      description: ['Privacy policy', 'Terms of use'],
-    },
-  ];
+  {
+    title: "About",
+    description: ["Team", "History", "Contact us", "Locations"],
+  },
+  {
+    title: "Features",
+    description: ["Cleaning"],
+  },
+  {
+    title: "Resources",
+    description: ["FAQ"],
+  },
+  {
+    title: "Legal",
+    description: ["Privacy policy", "Terms of use"],
+  },
+];
 
 export default function Footer() {
   return (
-   <>
+    <>
       <Container
         maxWidth="md"
         component="footer"
+        className={styles.Footer}
         sx={{
           mt: 2,
           py: [1, 1],
         }}
       >
-        {/* <Grid container spacing={4} justifyContent="space-evenly">
+        <div className={styles.FooterDiv}>
+          {/* <Grid container spacing={4} justifyContent="space-evenly">
           {footers.map((footer) => (
             <Grid item xs={1} sm={2} key={footer.title}>
               <Typography variant="h6" color="text.primary" gutterBottom>
@@ -73,46 +82,59 @@ export default function Footer() {
             </Grid>
           ))}
         </Grid> */}
-        <Grid container spacing={4} justifyContent="space-evenly" >
-
-            <Grid item xs={1} sm={2} key={'title'} className={styles.Grid}>
-                  <IconButton href="https://www.tiktok.com/@fine.spec" sx={{display: 'flex', justifyContent: 'center', width: '40px', height: '40px'}}>
-                    <TikTok color="white" />
-                  </IconButton>
+          <Grid container spacing={4} justifyContent="space-evenly">
+            <Grid item xs={1} sm={2} key={"tikTok"} className={styles.Grid}>
+              <IconButton
+                href="https://www.tiktok.com/@fine.spec"
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  width: "40px",
+                  height: "40px",
+                }}
+              >
+                <TikTok color="white" />
+              </IconButton>
             </Grid>
-        
-            <Grid item xs={1} sm={2} key={'title'} className={styles.Grid}>
-              <IconButton href='https://m.facebook.com/finespecdetailing' sx={{display: 'flex', justifyContent: 'center'}}>
+
+            <Grid item xs={1} sm={2} key={"faceBook"} className={styles.Grid}>
+              <IconButton
+                href="https://m.facebook.com/finespecdetailing"
+                sx={{ display: "flex", justifyContent: "center" }}
+              >
                 <FacebookIcon />
               </IconButton>
-        
             </Grid>
 
-            <Grid item xs={1} sm={2} key={'title'} className={styles.Grid}>
-              <IconButton href='https://www.instagram.com/fine.spec/' sx={{display: 'flex', justifyContent: 'center'}}>
-                  <InstagramIcon />
+            <Grid item xs={1} sm={2} key={"Insta"} className={styles.Grid}>
+              <IconButton
+                href="https://www.instagram.com/fine.spec/"
+                sx={{ display: "flex", justifyContent: "center" }}
+              >
+                <InstagramIcon />
               </IconButton>
-              
             </Grid>
-            <Grid item xs={1} sm={2} key={'title'} className={styles.Grid}>
-              <IconButton href="mailto:finespecdetailing@gmail.com" sx={{display: 'flex', justifyContent: 'center'}}>
+            <Grid item xs={1} sm={2} key={"eMail"} className={styles.Grid}>
+              <IconButton
+                href="mailto:finespecdetailing@gmail.com"
+                sx={{ display: "flex", justifyContent: "center" }}
+              >
                 <EmailIcon />
               </IconButton>
             </Grid>
-     
-        
-            <Grid item xs={1} sm={2} key={'title'} className={styles.Grid}>
-              <IconButton href="tel:484-655-9375" sx={{display: 'flex', justifyContent: 'center'}}>
+
+            <Grid item xs={1} sm={2} key={"phone"} className={styles.Grid}>
+              <IconButton
+                href="tel:484-655-9375"
+                sx={{ display: "flex", justifyContent: "center" }}
+              >
                 <PhoneIphoneIcon />
               </IconButton>
             </Grid>
-        
-         
-     
-        
-        </Grid>
-        <Copyright sx={{ mt: 5 }} />
+          </Grid>
+          <Copyright sx={{ mt: 2, mb: 2 }} />
+        </div>
       </Container>
-   </>
-  )
+    </>
+  );
 }
