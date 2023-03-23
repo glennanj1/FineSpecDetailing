90427cd8b29ef3e16646dbfceb05f71ed871d31c
glennanj1 <glennan20@gmail.com>
2023-03-23 00:32:25 -0400

    made alert dynamic

--- finespecdetailing/Components/Alert.js
+++ finespecdetailing/Components/Alert.js
@@ -1,14 +1,29 @@
-import * as React from 'react';
-import Alert from '@mui/material/Alert';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import Alert from "@mui/material/Alert";
+import { AlertTitle } from "@mui/material";
+import Stack from "@mui/material/Stack";
 
-export default function ActionAlerts() {
-  return (
-    <Stack sx={{ width: '100%', position: 'fixed', top: 0 }} spacing={2}>
-      <Alert onClose={() => {}}>
-            Successfully Booked!
+export default function ActionAlerts({ type, message }) {
+  if (type === "success")
+    return (
+      <Stack
+        sx={{ width: "100%", position: "fixed", top: 0, left: 0 }}
+        spacing={2}
+      >
+        <Alert variant="filled" severity="success" onClose={() => {}}>
+          <AlertTitle>Success</AlertTitle>
+          {message}
+        </Alert>
+      </Stack>
+    );
+  if (type === "error") {
+    <Stack
+      sx={{ width: "100%", position: "fixed", top: 0, left: 0 }}
+      spacing={2}
+    >
+      <Alert variant="filled" severity="error" onClose={() => {}}>
+        <AlertTitle>Error</AlertTitle>
+        {message}
       </Alert>
-    </Stack>
-  );
+    </Stack>;
+  }
 }
