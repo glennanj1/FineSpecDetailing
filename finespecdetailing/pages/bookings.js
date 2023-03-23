7e92d46511ce6a43e3066defdcc41f2dea202a43
glennanj1 <glennan20@gmail.com>
2023-03-23 00:34:53 -0400

    width adjustment

--- finespecdetailing/pages/bookings.js
+++ finespecdetailing/pages/bookings.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Container from '@mui/material/Container'
 import styles from '../Styles/Book.module.css';
 import AllBookings from '../Components/AllBookings';
@@ -6,12 +5,10 @@ import AllBookings from '../Components/AllBookings';
 
 export default function Bookings() {
   return (
-    <>
-    <Container maxWidth="md">
+    <Container maxWidth="xl">
       <main className={styles.main}>        
         <AllBookings />
       </main>
     </Container>
-  </>
   )
 }
