48133bb56d7a5bba6fd208971b1a7a88e6b279e0
glennanj1 <glennan20@gmail.com>
2023-03-23 00:32:57 -0400

    admin bookings and adjusted client bookings

--- finespecdetailing/Components/AllBookings.js
+++ finespecdetailing/Components/AllBookings.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { useSession, getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import DataGrid from "./BookingTable";
 import Container from "@mui/material/Container";
-import Button from '@mui/material/Button'
-import Progress from '../Components/Progress.js'
-import BookingCards from '../Components/BookingCards.js'
-import Box from "@mui/material/Box";
-
+import Button from "@mui/material/Button";
+import BookingCards from "../Components/BookingCards.js";
+import Spinner from "./Spinner";
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/system";
 
 export default function AllBookings() {
   const { data: session, status } = useSession();
   const [bookings, setBookings] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [noBooking, setNoBooking] = useState(false);
+
   useEffect(() => {
     fetch("/api/bookings/")
       .then((res) => {
@@ -24,61 +26,78 @@ export default function AllBookings() {
       })
       .then((data) => {
         setLoading(false);
+        if (!data.data.length >= 1) setNoBooking(true);
         setBookings(data.data);
       })
       .catch((e) => {
         console.debug(e);
         setLoading(false);
       });
-      console.log(session);
+    console.log(session);
   }, [setBookings, setLoading]);
 
-  if (status === "loading") {
-    return (
-      <Container component="main" maxWidth="md" style={{height: '30vh', display: 'flex', justifyContent: 'center', alignText: 'center', alignContent: 'center'}}>
-        <h1>Loading ...</h1>
-        <Progress />
-      </Container>
-    );
-  }
+  if (status === "loading") return <Spinner />;
+
   if (status === "unauthenticated") {
     return (
-      <Container component="main" maxWidth="md" style={{height: '30vh', display: 'flex', justifyContent: 'center', alignText: 'center', alignContent: 'center'}}>
-     
+      <Container
+        component="main"
+        maxWidth="lg"
+        style={{
+          height: "30vh",
+          display: "flex",
+          justifyContent: "center",
+          alignText: "center",
+          alignContent: "center",
+        }}
+      >
         <h1>Please Login to Continue</h1>
-        <Button onClick={() => signIn()} variant="outlined" sx={{ my: 1, mx: 1.5 }} className={styles.Font}>
+        <Button
+          onClick={() => signIn()}
+          variant="outlined"
+          sx={{ my: 1, mx: 1.5 }}
+          className={styles.Font}
+        >
           Login
         </Button>
-    
       </Container>
     );
   }
   if (status === "authenticated") {
+    if (noBooking)
+      return (
+        <>
+          <Container
+            component="main"
+            maxWidth="false"
+            style={{ height: "50vh" }}
+          >
+            <h2>Currently No Bookings At this Time!</h2>{" "}
+            <Button variant="contained" color="primary" href="/book">
+              Book Now
+            </Button>
+          </Container>
+        </>
+      );
+
+    if (session.status === "admin") return <DataGrid />;
+
     return (
       <>
-        {session.status === "admin" ? (
-          <DataGrid />
-        ) : (
-          <>
-              <h1>Your Bookings:</h1>
-              {!loading && bookings.length >= 1 ? (
-                <Box sx={{ minWidth: 275, maxWidth: 500, padding: 10 }}>
-                  {bookings?.map((book) => {
-                      return <BookingCards booking={book}/>
-                  })}
-                </Box>
-            ) : (
-              <>
-                <Container component="main" maxWidth="md" style={{height: '50vh'}}>
-                  <h2>Currently No Bookings At this Time!</h2>{" "}
-                  <Button variant="contained" color="primary" href="/book">
-                    Book Now
-                  </Button>
-                </Container>
-              </>
-            )}
-          </>
-        )}
+        <h1>Your Bookings:</h1>
+        <Box sx={{flexGrow: 1, height: '80%'}}>
+            <Grid container spacing={6} >
+          {bookings
+          .sort((a, b) => a.appointment > b.appointment ? -1 : 1)
+          .map((book) => {
+            return(
+              <Grid item sm={4} xs={12}>
+                <BookingCards booking={book} />
+              </Grid>
+            );
+          })}
+            </Grid>
+        </Box>
       </>
     );
   }
